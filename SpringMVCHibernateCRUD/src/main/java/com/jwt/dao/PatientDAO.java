b147f16c38e2931cda7d6ffe7452ed078cc8cf0f
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-21 11:15:59 +0530

    patient

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/PatientDAO.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/PatientDAO.java
@@ -1,5 +1,20 @@
 package com.jwt.dao;
 
+import java.util.List;
+
+import com.jwt.model.Patient;
+
 public interface PatientDAO {
 
+	public void addPatient(Patient patient);
+
+	public List<Patient> getAllPatients();
+
+	public void deletePatient(Integer PatientId);
+
+	public Patient getPatient(int PatientId);
+
+	public Patient updatePatient(Patient patient);
+
+	
 }
