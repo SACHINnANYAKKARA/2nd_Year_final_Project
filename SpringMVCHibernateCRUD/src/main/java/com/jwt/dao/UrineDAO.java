47513ae43e7d9d2e2c55a73fef6a5e842163c029
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-22 09:54:25 +0530

    bllod report

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/UrineDAO.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/UrineDAO.java
@@ -1,5 +1,17 @@
 package com.jwt.dao;
 
-public interface UrineDAO {
+import java.util.List;
 
+import com.jwt.model.Urine;
+
+public interface UrineDAO {
+	public void addUrine(Urine urine);
+
+	public List<Urine> getAllUrines();
+
+	public void deleteUrine(Integer urineId);
+
+	public Urine getUrine(int urineid);
+
+	public Urine updateUrine(Urine urine);
 }
