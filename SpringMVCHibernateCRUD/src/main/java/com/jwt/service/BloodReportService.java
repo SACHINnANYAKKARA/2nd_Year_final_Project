b10e9677d81e6384946dffb2b4b944e047ef38c8
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-24 19:09:15 +0530

    patient diagonsis

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/service/BloodReportService.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/service/BloodReportService.java
@@ -2,18 +2,18 @@ package com.jwt.service;
 
 import java.util.List;
 
-import com.jwt.model.Blood_Report;
+import com.jwt.model.BloodReport;
 
 public interface BloodReportService {
 
-	public void addBloodReport(Blood_Report blood_Report);
+	public void addBloodReport(BloodReport blood_Report);
 
-	public List<Blood_Report> getAllBloodReports();
+	public List<BloodReport> getAllBloodReports();
 
 	public void deleteBloodReports(Integer BloodReportId);
 
-	public Blood_Report getBloodReports(int BloodReportId);
+	public BloodReport getBloodReports(int BloodReportId);
 
-	public Blood_Report updateBloodReport(Blood_Report blood_Report);
+	public BloodReport updateBloodReport(BloodReport blood_Report);
 	
 }
