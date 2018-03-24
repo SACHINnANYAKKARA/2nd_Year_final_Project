1dd80b4a4e004ada6ada2c758e29f9072964477d
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-24 18:10:12 +0530

    patient diagonsis

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/PatientDiagnosisDAO.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/PatientDiagnosisDAO.java
@@ -1,5 +1,18 @@
 package com.jwt.dao;
 
+import java.util.List;
+
+import com.jwt.model.PatientDiagnosis;
+
 public interface PatientDiagnosisDAO {
 
+	public void addPatientDiagnosis(PatientDiagnosis diagnosis);
+
+	public List<PatientDiagnosis> getAllPatientDiagnosis();
+
+	public void deletePatientDiagnosis(Integer PatientDiagnosisID);
+
+	public PatientDiagnosis getPatientDiagnosis(int PatientDiagnosisID);
+
+	public PatientDiagnosis updatePatientDiagnosis(PatientDiagnosis patientDiagnosis);
 }
--- SpringMVCHibernateCRUD/src/main/java/com/jwt/service/PatientDiagnosisService.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/service/PatientDiagnosisService.java
@@ -1,5 +1,19 @@
 package com.jwt.service;
 
+import java.util.List;
+
+import com.jwt.model.PatientDiagnosis;
+
 public interface PatientDiagnosisService {
 
+	public void addPatientDiagnosis(PatientDiagnosis diagnosis);
+
+	public List<PatientDiagnosis> getAllPatientDiagnosis();
+
+	public void deletePatientDiagnosis(Integer PatientDiagnosisID);
+
+	public PatientDiagnosis getPatientDiagnosis(int PatientDiagnosisID);
+
+	public PatientDiagnosis updatePatientDiagnosis(PatientDiagnosis patientDiagnosis);
+	
 }
