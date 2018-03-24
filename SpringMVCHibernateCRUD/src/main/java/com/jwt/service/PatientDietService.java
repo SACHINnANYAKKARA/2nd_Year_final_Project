fddd36db494c5c5b068d974a6ecfcfd69ce9fc97
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-24 19:48:19 +0530

    patient Diet

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/service/PatientDietService.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/service/PatientDietService.java
@@ -1,5 +1,19 @@
 package com.jwt.service;
 
+import java.util.List;
+
+import com.jwt.model.PatientDiet;
+
 public interface PatientDietService {
 
+	public void addPatientDiet(PatientDiet patientDiet);
+
+	public List<PatientDiet> getAllPatientDiets();
+
+	public void deletePatientDiets(Integer PatientDietId);
+
+	public PatientDiet getPatientDiets(int PatientDietId);
+
+	public PatientDiet updatePatientDiet(PatientDiet patientDiet);
+	
 }
