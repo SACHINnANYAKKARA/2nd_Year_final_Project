4ae33e7d530f7f08672c49fe0d425a885ec733f6
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-25 20:01:45 +0530

    patient diagonsis

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/service/PatientDietServiceImpl.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/service/PatientDietServiceImpl.java
@@ -1,5 +1,55 @@
 package com.jwt.service;
 
-public class PatientDietServiceImpl {
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.jwt.dao.PatientDietDAO;
+import com.jwt.model.PatientDiet;
+
+
+@Service
+@Transactional
+
+public class PatientDietServiceImpl implements PatientDietService {
+
+	@Autowired
+	private PatientDietDAO patientDietDAO;
+
+	@Transactional
+	public void addPatientDiet(PatientDiet patientDiet) {
+		patientDietDAO.addPatientDiet(patientDiet);
+		
+	}
+
+	@Transactional
+	public List<PatientDiet> getAllPatientDiets() {
+		return patientDietDAO.getAllPatientDiets();
+	}
+
+	@Transactional
+	public void deletePatientDiets(Integer PatientDietId) {
+		patientDietDAO.deletePatientDiets(PatientDietId);
+		
+	}
+
+	@Override
+	public PatientDiet getPatientDiets(int PatientDietId) {
+		return patientDietDAO.getPatientDiets(PatientDietId);
+	}
+
+	@Override
+	public PatientDiet updatePatientDiet(PatientDiet patientDiet) {
+		return patientDietDAO.updatePatientDiet(patientDiet);
+	}
+
+	public void setPatientDietDAO(PatientDietDAO patientDietDAO) {
+		this.patientDietDAO = patientDietDAO;
+	}
+
+	
 }
