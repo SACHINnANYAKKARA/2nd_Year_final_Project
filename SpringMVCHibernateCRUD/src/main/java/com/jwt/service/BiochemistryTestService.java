1eeb223813ffc802b67ad2e3ae3ed4dbaadf9e88
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-26 18:46:53 +0530

    biochemistry_test

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/service/BiochemistryTestService.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/service/BiochemistryTestService.java
@@ -1,5 +1,20 @@
 package com.jwt.service;
 
-public class BiochemistryTestService {
+import java.util.List;
 
+import com.jwt.model.BiochemistryTest;
+
+public interface BiochemistryTestService {
+
+	public void addBiochemistryTest(BiochemistryTest biochemistryTest);
+
+	public List<BiochemistryTest> getAllBiochemistryTests();
+
+	public void deleteBiochemistryTest(Integer BiochemistryTestId);
+
+	public BiochemistryTest getBiochemistryTest(int BiochemistryTestid);
+
+	public BiochemistryTest updateBiochemistryTest(BiochemistryTest biochemistryTest);
+	
+	
 }
