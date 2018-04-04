cdd4d1e2b4ee28301cfc8e32b36b377cc5ea6c86
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-04-05 01:08:09 +0530

    Appoinment

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/DoctorDAO.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/dao/DoctorDAO.java
@@ -1,5 +1,19 @@
 package com.jwt.dao;
 
+import java.util.List;
+
+import com.jwt.model.Doctor;
+
 public interface DoctorDAO {
+	
+	public void addDoctors(Doctor doctor);
+
+	public List<Doctor> getAllDoctor();
+
+	public void deleteDoctor(Integer DoctorID);
+
+	public Doctor getDoctor(int  DoctorID);
+
+	public  Doctor updateDoctor(Doctor doctor);
 
 }
